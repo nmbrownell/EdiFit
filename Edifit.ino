16c00305b124c20c10323b3bb45cf33f0a049c23
Unknown <nmbrownell1@gmail.com>
2017-12-13 14:21:13 -0600

    RTC and BT

--- Edifit.ino
+++ Edifit.ino
@@ -6,6 +6,7 @@
 #define chargeLED 4
 #define powerLED 5
 #define baud 9600
+#define delim "="
 
 // Define state machine values
 enum State
@@ -16,32 +17,34 @@ enum State
   READ_TEMP,
   READ_ACCELEROMETER,
   READ_GPS,
+  READ_TIME,
   HOUSEKEEPING,
   BLUETOOTH_CHECK_IN,
 };
 
 // Fields
 
-/*
- * Byte 0: Is data refreshed. Incomming command will set to 1. Set to 0 when used.
- * Byte 1: Run command 0/1
-*/
+// Parameters sent from phone
 char dataReceived[100];
 
-/*
- * Byte 0: Used by bluetooth master. Always set to 1
- * Byte 1: Is running
- * Bytes 2 - 6: Force sensor 1 reading in Kg 
- * Bytes 7 - 11: Force sensor 2 reading in Kg
- * Bytes 12 - 14: Heart rate in BPM
- * Bytes 15 - 17: Temperature of Battery in C
- * Bytes 18 - 20: Temperature of CPU in C
- * Bytes 21 - 44: Accelerometer readings: 8 Bytes each for x y z 
- * Bytes 45 - 60: GPS readings: 8 Bytes each for lattitude and Longitude
-*/
-char dataToSend[100];
+// Parameters to be sent to phone
+int isRunning = 0;
+int force1 = 0;
+int force2 = 0;
+int heartRate = 0;
+int batTemp = 0;
+int cpuTemp = 0;
+int accelX = 0;
+int accelY = 0;
+int accelZ = 0;
+int gpsLat = 0;
+int gpsLong = 0;
+int secs = 0;
+int mins = 0;
+int hrs = 0;
+int days = 0;
 State state =  STOP;
-char runCommand = '0'; //Bluetooth sends ASCII, so store as char
+int runCommand = '0'; //Bluetooth sends ASCII, so store as char
 
 
 // Initialize
@@ -49,9 +52,11 @@ void setup()
 {
   // Setup Bluetooth:
   Serial.begin(baud);
-
+ 
   // Setup I2C bus:
   Wire.begin();
+  setDS3231time(0,0,0,0,0,0,0);
+
 }
 
 void loop()
@@ -59,49 +64,115 @@ void loop()
   switch (state)
   {
     case STOP:
-      //delay(100); // No OP
+      delay(1000); // No OP
       state = BLUETOOTH_CHECK_IN;
     break;
     
     case READ_FORCE:
+      state = READ_HEART;
     break;
     
     case READ_HEART:
+      state = READ_TEMP;
     break;
     
     case READ_TEMP:
+      state = READ_ACCELEROMETER;
     break;
     
     case READ_ACCELEROMETER:
+      state = READ_GPS;
     break;
     
     case READ_GPS:
+      state = READ_TIME;
+    break;
+    
+    case READ_TIME:
+      byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+      // retrieve data from DS3231
+      readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
+      
+      secs = second;
+      mins = minute;
+      hrs = hour;
+      days = dayOfMonth;
+      
+      
+      state = HOUSEKEEPING;
     break;
     
     case HOUSEKEEPING:
+      state = BLUETOOTH_CHECK_IN;
     break;
     
     case BLUETOOTH_CHECK_IN:
       if(Serial.available() > 0)
       {
         Serial.readBytesUntil('\0', dataReceived, 100);
-        if (dataReceived[0] == '1')
-        {
-          Serial.print("The message was ");
-          Serial.println (dataReceived);
-        }
       }
+      if (dataReceived[0] == '1')
+      {
+        writeParams();
+        state = READ_FORCE;
+      }
+      else if(dataReceived[0] = '0')
+      {
+        state = STOP;
+      }
+      else
+      {
+        state = STOP;
+      }
+      delay(1000);
       
-    
     break;
     
     default:
       Serial.write("Illegal state");
+      state = READ_FORCE;
     break;
   }
 }
 
 /*Region: Helper Methods*/
+
+/*Bluetooth*/
+void writeParams()
+{
+  Serial.print(isRunning);
+  Serial.print(delim);
+  Serial.print(force1);
+  Serial.print(delim);
+  Serial.print(force2);
+  Serial.print(delim);
+  Serial.print(heartRate);
+  Serial.print(delim);
+  Serial.print(batTemp);
+  Serial.print(delim);
+  Serial.print(cpuTemp);
+  Serial.print(delim);
+  Serial.print(accelX);
+  Serial.print(delim);
+  Serial.print(accelY);
+  Serial.print(delim);
+  Serial.print(accelZ);
+  Serial.print(delim);
+  Serial.print(gpsLat);
+  Serial.print(delim);
+  Serial.print(gpsLong);
+  Serial.print(delim);
+  Serial.print(days);
+  Serial.print(delim);
+  Serial.print(hrs);
+  Serial.print(delim);
+  Serial.print(mins);
+  Serial.print(delim);
+  Serial.print(secs);
+  Serial.println("");
+}
+/*End Bluetooth*/
+
 
 /*RTC*/
 
@@ -116,7 +187,51 @@ void loop()
 
 
 /*RTC*/
-
+byte decToBcd(byte val)
+{
+  return( (val/10*16) + (val%10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+byte bcdToDec(byte val)
+{
+  return( (val/16*10) + (val%16) );
+}
+void setDS3231time(byte second, byte minute, byte hour, byte dayOfWeek, byte
+dayOfMonth, byte month, byte year)
+{
+  // sets time and date data to DS3231
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set next input to start at the seconds register
+  Wire.write(decToBcd(second)); // set seconds
+  Wire.write(decToBcd(minute)); // set minutes
+  Wire.write(decToBcd(hour)); // set hours
+  Wire.write(decToBcd(dayOfWeek)); // set day of week (1=Sunday, 7=Saturday)
+  Wire.write(decToBcd(dayOfMonth)); // set date (1 to 31)
+  Wire.write(decToBcd(month)); // set month
+  Wire.write(decToBcd(year)); // set year (0 to 99)
+  Wire.endTransmission();
+}
+void readDS3231time(byte *second,
+byte *minute,
+byte *hour,
+byte *dayOfWeek,
+byte *dayOfMonth,
+byte *month,
+byte *year)
+{
+  Wire.beginTransmission(DS3231_I2C_ADDRESS);
+  Wire.write(0); // set DS3231 register pointer to 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS3231_I2C_ADDRESS, 7);
+  // request seven bytes of data from DS3231 starting from register 00h
+  *second = bcdToDec(Wire.read() & 0x7f);
+  *minute = bcdToDec(Wire.read());
+  *hour = bcdToDec(Wire.read() & 0x3f);
+  *dayOfWeek = bcdToDec(Wire.read());
+  *dayOfMonth = bcdToDec(Wire.read());
+  *month = bcdToDec(Wire.read());
+  *year = bcdToDec(Wire.read());
+}
 /*End RTC*/
 
 
